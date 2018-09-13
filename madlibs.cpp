03a4a25131a7cb1752c03dd759a9ad61fa14d022
Omarbojo <obojorquez@mail.csuchico.edu>
2018-09-12 22:53:25 -0700

    I made my own mad libs game

--- madlibs.cpp
+++ madlibs.cpp
@@ -1,9 +1,69 @@
 //Authors: 
 #include <iostream>
 #include <string>
+using namespace std;
 
 int main()
 {
+
+  string one= "";
+  int two;
+  string thre = "";
+  string four = "";
+  string five = "";
+  string six = "";
+  string seven = "";
+  string eight = "";
+  string nine = "";
+  string ten = "";
+
+  std::cout<<"Enter a noun"<<std::endl;
+  std::cin>>one;
+  std::cout<<"Enter a number"<<std::endl;
+  std::cin>>two;
+  std::cout<<"Enter an adjective"<<std::endl;
+  std::cin>>thre;
+  std::cout<<"Enter an adjective"<<std::endl;
+  std::cin>>four;
+  std::cout<<"Enter a noun"<<std::endl;
+  std::cin>>five;
+  std::cout<<"Enter a adjective"<<std::endl;
+  std::cin>>six;
+  std::cout<<"Enter a noun"<<std::endl;
+  std::cin>>seven;
+  std::cout<<"Enter a feeling"<<std::endl;
+  std::cin>>eight;
+  std::cout<<"Enter a past tense verb"<<std::endl;
+  std::cin>>nine;
+  std::cout<<"Enter an adjective"<<std::endl;
+  std::cin>>ten;
+
+  std::cout<<"Princess ";
+  std::cout<<one;
+  std::cout<<" had just finished eating ";
+  std::cout<<two;
+  std::cout<<" very ";
+  std::cout<<thre;
+  std::cout<<" sandwhiches."<<endl;
+  std::cout<<"She was feeling ";
+  std::cout<<four;
+  std::cout<<" and decided to go watch sir ";
+  std::cout<<five;
+  std::cout<<" joust his ";
+  std::cout<<six;
+  std::cout<<" rival the ";
+  std::cout<<seven;
+  std::cout<<" Knight."<<endl;
+  std::cout<<"As she watched she got very ";
+  std::cout<<eight;
+  std::cout<<" and ";
+  std::cout<<nine;
+  std::cout<<"  until she threw up all her  ";
+  std::cout<<ten;
+  std::cout<<"  lunch."<<endl;
+
+
+
 
   return 0;
 }
